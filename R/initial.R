4cec0682fddc65d0d8038569a3d87134f5e3863b
timriffe <tim.riffe@gmail.com>
2021-08-20 15:16:01 +0200

    fixes teeny tiny typo

--- R/initial.R
+++ R/initial.R
@@ -1,10 +1,10 @@
 #' Title initial
 #'
-#' @return The message 'Welcome to the rrddharmony package'
+#' @return The message 'Welcome to the rddharmony package'
 #' @export
 #'
 #' @examples
 #' initial()
 initial <- function(){
-  print("Welcome to the rrddharmony package")
+  print("Welcome to the rddharmony package")
 }
