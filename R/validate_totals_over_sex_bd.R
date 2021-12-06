ed1efaddd12b3b9a5200bec68e2e1a7d64026703
Shelmith-Kariuki <kariukishelmith@gmail.com>
2021-12-06 09:23:38 +0300

    Edits as of 6th Dec at 9:23 am

---
+++ R/validate_totals_over_sex_bd.R
@@ -0,0 +1,10 @@
+#' validate_totals_over_sex_bd
+#'
+#' This dataset has been used to show how dd_validate_totals_over_sex() works.
+#'
+#' @format A data frame with 360 rows and 48 variables.
+#'
+#' \describe{
+#'   ...
+#' }
+"validate_totals_over_sex_bd"
---
+++ R/validate_totals_over_sex_pop.R
@@ -0,0 +1,10 @@
+#' validate_totals_over_sex_pop
+#'
+#' This dataset has been used to show how dd_validate_totals_over_sex_new() works.
+#'
+#' @format A data frame with 63 rows and 46 variables.
+#'
+#' \describe{
+#'   ...
+#' }
+"validate_totals_over_sex_pop"
